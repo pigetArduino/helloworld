9c1ba4f42716e0d0432c2fc52aa88990d01edefe
maditnerd <remi@madnerd.org>
2016-05-19 21:16:31 +0200

    First autocommit by pigetBot

---
+++ helloworld/helloworld.ino
@@ -0,0 +1,14 @@
+// Arduino Serial Example
+// Open Serial Monitor (Tools/Serial Monitor)
+// github.com/pigetArduino/helloworld
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(9600); //Start serial communication at 9600
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  Serial.println("Hello World"); //Print hello World
+  delay(2000); //Wait 2 seconds
+}
